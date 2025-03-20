edb1510b422399b96a7718dedde70b42f7d29434
Simchanmin <simcm20003@gmail.com>
2025-03-20 12:06:00 +0900

    are

--- java_main/src/App.java
+++ java_main/src/App.java
@@ -1,11 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int npmt1 = 1;
-        int npmt2 = 2;
-        int npmt3 = npmt1+npmt2;
-
-
-        System.out.println(npmt3);
+        System.out.println("Hello, World!");
     }
 }
-  
